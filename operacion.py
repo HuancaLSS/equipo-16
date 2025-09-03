d9403aa8892435c5eb3f2e814e9f2067e55ba568
TitoS <titosoria4000@gmail.com>
2025-09-03 11:39:48 -0400

    mi archivo operacion python

---
+++ operacion.py
@@ -0,0 +1,27 @@
+# Operación matemática
+a = 11
+b = 5
+resultado = a + b
+
+# Mostrar resultado de la operación
+print("a:", a,"b:",b)
+print("La sumas es:", resultado)
+
+# Datos de los integrantes del grupo
+integrantes = [
+    {"nombre": "Huanca Lopez Sergio Santiago", "rol": "Líder"},
+    {"nombre": "Serrano Balderrama Daniel", "rol": "Desarrollador No 1"},
+    {"nombre": "Soria Limachi Tito", "rol": "Desarrollador No 2"},
+    {"nombre": "Viscarra Arias Hernan", "rol": "Desarrollador No 3"}
+]
+
+# Mostrar datos de los integrantes
+print("\nDatos de los integrantes del grupo:")
+for integrante in integrantes:
+    print(f"Nombre: {integrante['nombre']}, Rol: {integrante['rol']}")
+
+
+
+
+
+
