45d549685fd21d3edec7180a429922b780b4b2d2
nanohernan <hviscarraa@fcpn.edu.bo>
2025-09-03 16:49:16 -0400

    Crear saludo.java
    archivo de saludo con java

---
+++ saludo.java
@@ -0,0 +1,9 @@
+public class Saludo {
+ public static void main(String[] args) {
+System.out.println("¡Hola, equipo!");
+ System.out.println("1 Hernán Viscarra Arias");
+ System.out.println("2 Daniel Serrano Balderrama");
+System.out.println("3 Daniel Serrano Balderrama");
+System.out.println("4 Sergio Santiago Huancayo Looez");
+ }
+}
