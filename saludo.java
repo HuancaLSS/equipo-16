603a5c97e990eebad99af7d0de3bed166154541b
titos <titosoria4000@gmail.com>
2025-09-24 02:40:22 -0400

    Se modifico la ultima linea

--- saludo.java
+++ saludo.java
@@ -4,6 +4,8 @@ System.out.println("¡Hola, equipo!");
  System.out.println("1 Hernán Viscarra Arias");
  System.out.println("2 Daniel Serrano Balderrama");
 System.out.println("3 Daniel Serrano Balderrama");
-System.out.println("4 Sergio Santiago Huancayo Looez");
+System.out.println("4 Sergio Santiago Huancayo Looez");
+System.out.println("4 Sergio Santiago Huancayo Lopez");
+/* Se modifico la ultima linea */
  }
 }
